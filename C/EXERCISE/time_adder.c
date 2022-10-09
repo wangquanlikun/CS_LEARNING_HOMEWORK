bd5f5fa9079838f7797d1abf40499509f4702e15
Wangquanlikun <75524809+wangquanlikun@users.noreply.github.com>
2022-10-09 16:31:54 +0800

    Create time_adder.c

---
+++ C/EXERCISE/time_adder.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int time,add;
+    scanf("%d%d",&time,&add);
+    int hour,minute;
+    hour=time/100;
+    minute=time-hour*100;
+    int addminute=minute+add;
+    int hourplus,minuteplus,hour2,minute2;
+    while(addminute<0)
+    {
+        hour=hour-1;
+        addminute=addminute+60;
+    }
+    hourplus=addminute/60;
+    minuteplus=addminute-hourplus*60;
+    hour2=hour+hourplus;
+    minute2=minuteplus;
+    if(minute2!=0)
+        printf("%d%d",hour2,minute2);
+    else if(minute2==0)
+        printf("%d00",hour2);
+    return 0;
+}
