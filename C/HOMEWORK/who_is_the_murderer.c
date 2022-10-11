804904e84e8f39654f687ef390465e8c4878bbd3
Wangquanlikun <75524809+wangquanlikun@users.noreply.github.com>
2022-10-11 20:54:42 +0800

    Create who_is_the_murderer.c

---
+++ C/HOMEWORK/who_is_the_murderer.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i;//i是凶手的号码
+    int t = 0; //说真话的人数，要与n相等
+    int a[5],b[5];
+    int p;
+    for (p = 1; p <= 5;p++)
+    {
+        scanf("%d%d", &a[p], &b[p]);
+    }
+    for (i = 5; i >= 5 && i <= 9;i++)//从5开始轮查，假设此时i是凶手
+    {
+        t = 0;//注意要让其初始化
+        int q;
+        for (q = 1; q <= 5;q++)
+        {
+            if((i==a[q]&&b[q]==1)||(i!=a[q]&&b[q]==0))
+            {
+                t = t + 1;
+            }
+        }
+        if(t==n)
+        {
+            printf("%d", i);
+            break;
+        }
+    }
+    return 0;
+}
