73c828598671bd770f3af49dcba2d6728f248e7b
Wang Quan <75524809+wangquanlikun@users.noreply.github.com>
2022-11-11 22:20:16 +0800

    Update alphabet_statistics.c

--- C/HOMEWORK/alphabet_statistics.c
+++ C/HOMEWORK/alphabet_statistics.c
@@ -2,17 +2,17 @@
 #include<stdio.h>
 int main()
 {
-    int a[128]={0};
-    char input[101];
+    int a[128]={0};//数组位数为其ASCII码对应的字符，各位数据为该字符个数。初始化为0
+    char input[101];//字符型数组读入字符串
     scanf("%s",input);
     int i;
     for(i=0;i<=100&&input[i]!='\0';i++)
     {
-        char c=input[i];
+        char c=input[i];//将c的ASCII码对应a位数
         a[c]++;
     }
     char ii;
-    for(ii='A';ii<='z';ii++)
+    for(ii='A';ii<='z';ii++)//ii对应ASCII码
     {
         if(a[ii]!=0)
             printf("The character %c has presented %d times.\n",ii,a[ii]);
